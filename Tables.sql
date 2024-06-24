b2c3c80cac6d60bd6df10f7066c84eb10962da04
YishaiDredzen <88730903+YishaiDredzen@users.noreply.github.com>
2024-06-24 16:16:56 +0300

    Stage 2

--- Tables.sql
+++ Tables.sql
@@ -62,3 +62,19 @@ CREATE TABLE CodeShare (
     Restrictions VARCHAR NOT NULL,
     FOREIGN KEY (FlightNumber) REFERENCES Flight(FlightNumber)
 );
+
+CREATE TABLE PassengerBooking (
+    PassengerID INT NOT NULL,
+    BookingID INT NOT NULL,
+    PRIMARY KEY (PassengerID, BookingID),
+    FOREIGN KEY (PassengerID) REFERENCES Passenger(PassengerID),
+    FOREIGN KEY (BookingID) REFERENCES Booking(BookingID)
+);
+
+CREATE TABLE BookingPackage (
+    BookingID INT,
+    PackageID INT,
+    PRIMARY KEY (BookingID, PackageID),
+    FOREIGN KEY (BookingID) REFERENCES Booking(BookingID),
+    FOREIGN KEY (PackageID) REFERENCES Package(PackageID)
+);
